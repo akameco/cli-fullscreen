27ca58492cb3eee40132c1a912e806babfea89bb
akameco <akameco.t@gmail.com>
2016-08-17 06:46:03 +0900

    fix typo

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.true(Object.keys(m).lnegth > 0);
+	t.true(Object.keys(m).length > 0);
 	t.is(typeof m.enter, 'function');
 	t.is(typeof m.leave, 'function');
 });
